9cea2e9278b785ddbdcd1261537a852d292fb771
DivyaM310 <mulikdivya1@gmail.com>
2024-06-09 17:17:51 +0530

    second

---
+++ WarehouseManagementSystem/ConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WarehouseManagementSystem
+{
+    class ConnectionString
+    {
+        public string DBConn = "Data Source=.;Initial Catalog=POS_DB;Integrated Security=True;Connect Timeout=30";
+       
+    }
+}
